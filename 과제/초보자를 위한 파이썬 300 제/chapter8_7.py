c633aeb08dbc8939ffd7c9ff1335f548f6b182c8
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-03 22:06:30 +0900

    study

--- 과제/초보자를 위한 파이썬 300 제/chapter8_7.py
+++ 과제/초보자를 위한 파이썬 300 제/chapter8_7.py
@@ -6,6 +6,74 @@ data = [
 ]
 for row in data:
     for col in row:
-        print(col*1.00014)
+        print(col * 1.00014)
+print()
+
+# Q.192
+for row in data:
+    for col in row:
+        print(col * 1.00014)
+    print('-' * 4)
+print()
+
+# Q.193
+result = []
+for row in data:
+    for col in row:
+        result.append(col * 1.00014)
+print(result)
+print()
+
+# Q.194
+result = []
+for row in range(len(data)):
+    sub = []
+    for col in range(len(data[row])):
+        sub.append(data[row][col] * 1.00014)
+    result.append(sub)
+print(result)
+print()
+
+# Q.195
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190], 
+        [300, 310, 300, 310]]
+for i in range(1, len(ohlc)):
+    print(ohlc[i][-1])
+print()
+
+# Q.196
+for i in range(1, len(ohlc)):
+    if (150 < ohlc[i][-1]):
+        print(ohlc[i][-1])
+print()
+
+# Q.197
+for i in range(1, len(ohlc)):
+    if (ohlc[i][0] <= ohlc[i][-1]):
+        print(ohlc[i][-1])
+print()
+
+# Q.198
+volatility = []
+for i in range(1, len(ohlc)):
+    volatility.append(ohlc[i][1] - ohlc[i][2])
+print(volatility)
+print()
+
+# Q.199
+volatility = 0
+for i in range(1, len(ohlc)):
+    if (ohlc[i][0] < ohlc[i][-1]):
+        volatility = ohlc[i][1] - ohlc[i][2]
+        print(volatility)
+print()
+
+# Q.200
+volatility = []
+for i in range(1, len(ohlc)):
+    volatility.append(ohlc[i][0] - ohlc[i][-1])
+print(sum(volatility))
 print()
 
