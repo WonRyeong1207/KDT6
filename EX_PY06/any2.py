f1829a2804ff188378384cb793a1fa7c9797b5ac
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-03 17:39:58 +0900

    day 6 study

--- EX_PY06/any2.py
+++ EX_PY06/any2.py
@@ -18,4 +18,27 @@ print('\n\n')
 n2, n3, n4, n5, n6, n7, n8, n9 = enumerate(dan)
 print(n2)
 '''
+'''
+with open('EX_PY06/words.txt', 'r') as word_file:
+    words_list = list(map(str, word_file.read().split()))
+    # print(words_list)
+    for i, d in enumerate(words_list):
+        words_list[i] = d.rstrip(',.')
+        print(words_list[i], end=' ')
+    print('n')
+    
+    for d in words_list:
+        if ('c' in d):
+            print(d)
+'''
+
+with open('EX_PY06/words.txt', 'r') as words_file:
+    word = words_file.readlines()
+    
+    for i, d in enumerate(word):
+        word[i] = d.rstrip('\n')
+    
+    for i in word:
+        if (i == i[::-1]):
+            print(i)
 
