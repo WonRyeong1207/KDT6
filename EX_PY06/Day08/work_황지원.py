39f823360496895f9b8bf14c9640107f6258e316
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-08 00:47:56 +0900

    day8 study

--- EX_PY06/Day08/work_황지원.py
+++ EX_PY06/Day08/work_황지원.py
@@ -132,6 +132,10 @@ def get_average(korean=0, english=0, mathematics=0, science=0):
         cnt += 1
     return (korean + english + mathematics + science)/cnt
 
+# type error
+# def get_average(*args):
+#     return sum(args)/len(args)
+
 min_score, max_score = get_min_max_score(korean, english, mathematics, science)
 average_score = get_average(korean=korean, english=english, mathematics=mathematics, science=science)
 print('낮은 점수: {0: .2f}, 높은 점수: {1: .2f}, 평균 점수: {2: .2f}'.format(min_score, max_score, average_score))
