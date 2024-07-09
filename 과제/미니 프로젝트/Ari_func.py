0d119b07a5e631f3b067b240eb27847021649f82
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-09 10:57:38 +0900

    병아리

--- 과제/미니 프로젝트/Ari_func.py
+++ 과제/미니 프로젝트/Ari_func.py
@@ -5,10 +5,34 @@
 import random
 
 # 대딥 리스트
-strat_positive_answer_list = ['안녕!', '대화하기를 선택해줘서 고마워.', '어떤 대화를 할까?']
-strat_soso_answer_list = ['안녕~', '다음에 다시 만나', 'ㅂㅂ', '나중에 봐']
-strat_negative_answer_list = ['잘 보고 입력한거 맞지?', '처음부터 대화할 생각이 없었던 건 아니고?', '허허']
+# 초기 화면
+strat_positive_answer_list = ['안녕!', '대화하기를 선택해줘서 고마워.', '어떤 대화를 할까?', '나는 병아리라고 해.', '반가워!',
+                              '나의 세계에 온것을 환영해~', '만나서 반가워.']
+strat_soso_answer_list = ['안녕~', '다음에 다시 만나', 'ㅂㅂ', '나중에 봐', '다음에도 놀러 와야해~ 꼭이야!!']
+strat_negative_answer_list = ['잘 보고 입력한 것 맞지?', '처음부터 대화할 생각이 없었던 건 아니고?', '허허. 왜이래?',
+                              '다른 입력키 넣으려다가 실수 한거지?', '그 정도로 키를 잘못 입력하면 좀 위험한거 아니야?']
 
+# 노말 화면, 노말 상태
+normal_hello_answer_list = ['안녕.', '안녕!', '안녕?','안녕~', '좋은 하루~', '좋은 날이네', '응']
+normal_call_answer_list = ['응?', '불렀어?', '왜?', '구래', '음?', '무슨 할 말 있어?', '하고 싶은 말 있어?', '급하게 안 불러도 되니까 천천히 말해도 괜찮아.',
+                           '몇 번을 부르는 거야?', '난 내이름 별로야. 그래서 다른 이름을 쓰고 있어.', '병아리 아닌데.. 아리인데...']
+normal_doing_answer_list = ['놀고 있었어.', '일하고 있었어.', '멍 때리고 있었어...', '피곤해서 자고 있었어.', '과제하고 있었어.', 
+                            '공부하고 있었어.', '쉬고 있었어.', '게임하고 있었어.', '영화보고 있었어.', '유튭보고 있었엉.', '사람들 만나고 있었어.',
+                            '모임이 있어서 밖이야.', '친구들이랑 놀고 있었어.', '아파서 병원에 다녀왔어. 단순 감기더라.', 'SNS하고 있었어. 요즘 뭐가 많네..']
+normal_withstudy_answer_list = ['음? 나도? 싫어 ㅎㅎ', '싫은데? 내가 왜?', '오늘 날씨 좋더라~ 화이팅!', '무슨 공부 하려고?',
+                                '좀 더 쉬다가 하면 안될까?', '그래.', '하기 싫어.. 싫다구.. ', '그럼 하루종일 공부하는 거지? 기대된다.',
+                                '그치 공부는 평소에도 해야지..', '꾸준한 것은 좋아!', '비오니까 하기 싫다.', '비가 오니까 뭔가 더 으슬으슬해서 집중이 잘되는 것 같은 느낌',
+                                '여기 나만 추운가? 다른 곳에서 할까?', '카페에서 떠들면서 공부하자!', '난 토론하면서 공부하는 방법이 좋아. 뭔가 더 기억에 잘 각인되는 느낌이고..',
+                                '몸이 안 좋아서 다음에 하면 안 될까?', '공부말고 다른 것이 하고 싶어.']
+normal_play_answer_list = ['그래그래.', '응? 지금?', '싫은데?', '싫은데...', '그래.', '구랭!', '워하고 놀려고?', '다음에 놀자', 
+                            '해야할 것들은 다 하고 노는 거지?', '그 정도로 놀고 싶은거야?', '피곤해서 다음에 놀자.', '할 것들이 많아서 다음에 놀자.',
+                            '피곤한데 나랑 놀아주는 거야?', '난 체스 좋아하는데 너는?', '']
+normal_hard_answer_list = ['오늘도 수고했어.', '고생했어.', '그 정도로 공부하면 당연히 힘들지.', '그 정도로 하면 함들지...',
+                           '요즘 많이 힘들지? 고생했어.', '요즘 많이 힘들지? 수고했어.']
+
+normal_bye_answer_list = ['ㅂㅂ', '다음에 또 대화하면 놀자.', '내일 봐~']
+normal_negative_answer_list = ['저기 잘보고 입력한거지?', '나랑 대화하고 싶은 거 맞지?', '가끔은 잘못 누를 수도 있지.. 암...',
+                               '저런 얼마나 피곤하면 저래...', '아냐.. 그냥 뭔가 섭섭하네.']
 
 
 
@@ -23,19 +47,70 @@ def input_check(key):
             return False
     else:
         False
-        
-# test
-# d = input("d : ")
-# print(input_check(d))
 
 # 입력을 받는 함수
 def input_key():
-    key = input("대답 (수를 입력) : ")
+    key = input("질문 번호를 입력 : ")
     if input_check(key):
         key = int(key)
+        print('\n\n\n')
         return key
     else:
+        print('\n\n\n')
         return None
+
+# 대답할 리스트를 선정하는 함수, 리스트의 이름이 길어서 보기 싫어서
+def select_list(state_num, answer_key=None): # else 고려
+    if state_num == 0:
+        if answer_key == 1:
+            return strat_positive_answer_list
+        elif answer_key == 2:
+            return strat_soso_answer_list
+        else:
+            return strat_negative_answer_list
+        
+    elif state_num == 1:
+        pass # 븅아리
+    
+    elif state_num == 2:
+        if answer_key == 0:
+            return normal_bye_answer_list
+        elif answer_key == 1:
+            return normal_hello_answer_list
+        elif answer_key == 2:
+            return normal_call_answer_list
+        elif answer_key == 3:
+            return normal_doing_answer_list
+        elif answer_key == 4:
+            return normal_withstudy_answer_list
+        elif answer_key == 5:
+            return normal_play_answer_list
+        elif answer_key == 6:
+            return normal_hard_answer_list
+        else:
+            return normal_negative_answer_list
+
+# 대답을 선택하는 함수
+def select_answer(answer_list):
+    talk = random.randint(0, len(answer_list)-1)
+    answer = answer_list[talk]
+    return answer
+
+# 대답을 보여주는 함수
+def print_answer(answer):
+    space = 100 - len(answer)*1.5
+    space = int(space)
+    print(f"{'*':*^100}")
+    print(f"* {'대답':^94} *")
+    print(f"{'*':*^100}")
+    print(f"{'*':<50}{'*':>50}")
+    print(f"* {answer:<{space}} *")
+    print(f"{'*':<50}{'*':>50}")
+    print(f"{'*':*^100}")
+    print('\n\n\n')
+    # 잠시 멈추게 하는 반복문
+    for _  in range(10000000):
+                    pass
 
 # 초기 화면 함수
 def start_ground():
@@ -49,13 +124,16 @@ def start_ground():
 # 대화를 시작했을때 상태창
 def normal_state_background():
     print(f"{'*':*^100}")
-    print(f"* {'1. 안녕.':< 80} *")
-    print(f"* {'2. 아리야.':<80} *")
-    print(f"* {'3. 워하고 있었어?':<80} *")
-    print(f"* {'4. 같이 공부하자.':<80} *")
-    print(f"* {'5. 같이 놀자!':<80} *")
-    print(f"* {'0. 시작화면으로 돌아가기':<80} *")
-    print(f"{'*':^100}")
+    print(f"* {'선택창':^93} *")
+    print(f"{'*':*^100}")
+    print(f"* {'1. 안녕.':<94} *")
+    print(f"* {'2. (정)병아리야.':<91} *")
+    print(f"* {'3. 워하고 있었어?':<90} *")
+    print(f"* {'4. 같이 공부하자.':<90} *")
+    print(f"* {'5. 같이 놀자!':<92} *")
+    print(f"* {'6. 힘들어...':<93} *")
+    print(f"* {'0. 시작화면으로 돌아가기':<86} *")
+    print(f"{'*':*^100}")
 
 # 대화 리스트 화면을 띄우는 함수
 def answer_background(state_num):
@@ -64,20 +142,20 @@ def answer_background(state_num):
     if state_num == 0:
         start_ground()
     
-    elif state_num == 1:
+    elif state_num == 1: # 이상한 상태의 (정)병아리 == 븅아리
         pass
     
     elif state_num == 2:
         normal_state_background()
         
 # test
-answer_background(0)
-
-answer_key = input_key()
-if answer_key == 1:
-    print(strat_positive_answer_list[random.randint(0, len(strat_positive_answer_list)-1)],'\n')
-elif answer_key == 2:
-    print(strat_soso_answer_list[random.randint(0, len(strat_soso_answer_list)-1)], '\n')
-else:
-    print(strat_negative_answer_list[random.randint(0, len(strat_negative_answer_list)-1)], '\n')
-
+if __name__=='__main__':
+    
+    # test
+    # d = input("d : ")
+    # print(input_check(d))
+    
+    # answer_background(2)
+    print_answer(select_answer(normal_withstudy_answer_list))
+    
+    
---
+++ 과제/미니 프로젝트/Ari_test.py
@@ -0,0 +1,72 @@
+# 함수가 너무 많아서 구동만 따로 하려함.
+import Ari_func as Ari
+import random # 위의 파일에 있는데 여기선 안쓰지만 없으면 이상한 폴더 생성됨.
+
+# 종료키 입력까지는 유지하고 있어야함. 종료키는 2
+while True:
+    Ari.answer_background(0)
+    answer_key = Ari.input_key()
+    
+    if answer_key == 1: # 대화시작
+        answer_list = Ari.select_list(0, 1)
+        talk = Ari.select_answer(answer_list)
+        Ari.print_answer(talk)
+        
+        # 이때도 계속 입력을 받아야함. 종료키는 0
+        while True:
+            Ari.answer_background(2)
+            answer_key = Ari.input_key()
+            
+            if answer_key == 0: # 시작화면으로 되돌아가기
+                answer_list = Ari.select_list(2, 0)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                break
+            
+            # 대답을 보고 넘어 갈 수 있도록 기다리는 키를 주고 싶음.
+            # for문 반복으로 잠시 멈출수 있도록함.
+            elif answer_key == 1: # 안녕
+                answer_list = Ari.select_list(2, 1)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            elif answer_key == 2: # 아리야
+                answer_list = Ari.select_list(2, 2)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            elif answer_key == 3: # 뭐하고 있어
+                answer_list = Ari.select_list(2, 3)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            elif answer_key == 4: # 같이 공부하자
+                answer_list = Ari.select_list(2, 4)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            elif answer_key == 5: # 놀자
+                answer_list = Ari.select_list(2, 5)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            elif answer_key == 6: # 힘들었어
+                answer_list = Ari.select_list(2, 6)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+                
+            else: # 다른 키 입력
+                answer_list = Ari.select_list(2)
+                talk = Ari.select_answer(answer_list)
+                Ari.print_answer(talk)
+        
+        
+    elif answer_key == 2: # 대화 종료
+        answer_list = Ari.select_list(0, 2)
+        talk = Ari.select_answer(answer_list)
+        Ari.print_answer(talk)
+        break
+    else: # 다른 키를 입력
+        answer_list = Ari.select_list(0)
+        talk = Ari.select_answer(answer_list)
+        Ari.print_answer(talk)
