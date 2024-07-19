851aa717e88d49ee31d3c81ed88687369c822b60
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-19 13:10:55 +0900

    메서드 추가 공부

--- EX_PANDAS06/Day02/ex_class_01.py
+++ EX_PANDAS06/Day02/ex_class_01.py
@@ -26,13 +26,25 @@ class Bugger:
         print(f"패 티 : {self.patty}")
         print(f"야 채 : {self.veg}")
         
+    # 속성을 변경하거나 읽어오는 메서드 => getter / setter 메서드
+    def get_bread(self):
+        return self.bread
+    
+    def set_bread(self, bread):
+        self.bread = bread
         
 # 클래스 사용하가
 # 객체를 만들고
 bugger1 = Bugger('브리오슈', '불고기', '양상추, 토마토, 양파')
+bugger2 = Bugger('참깨곡물빵', '쇠고기패티', '치즈, 양상추, 양파, 토마토')
+
 # 객체의 메소드 사용
 bugger1.print_info()
-print()
-
-bugger2 = Bugger('참깨곡물빵', '쇠고기패티', '치즈, 양상추, 양파, 토마토')
 bugger2.print_info()
+
+# 속성을 읽는 방법: 1. 직접 접근 읽기, 2.간접 접근 읽기 - getter 메서드 사용
+print(bugger1.bread, bugger1.get_bread())
+
+# 속성을 변경하는 방법 : 1`. 직접 접근 방법, 2. 간접 접근 방법 - setter 메서드 사용
+bugger1.bread = '들깨빵'
+bugger1.set_bread('올리브빵')
