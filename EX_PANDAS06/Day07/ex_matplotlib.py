33c4757286f4f5291c3ee0511326d4497f6094d6
WonRyeong1207 <hadesdis1207@gmail.com>
2024-07-23 00:50:21 +0900

    day7 study

---
+++ EX_PANDAS06/Day07/ex_matplotlib.py
@@ -0,0 +1,9 @@
+# 쥬피터가 아닌 파이썬 파일로
+
+# Matplotlib를 이용한 시각화
+
+import matplotlib.pyplot as plt
+
+# 그래프 그리기
+plt.plot([1, 2, 3, 4])
+plt.show()      # 없으면 안나옴.
---
+++ utils/visual.py
@@ -0,0 +1,12 @@
+# 시각화 관련 함수들 존재하는 모듈
+
+# 함수기는 : 버전체크 후 출려
+# 함수이름 : check_version
+# 매개변수 : None
+# 함수결과 : None
+
+def check_version():
+    import matplotlib
+    print(f"matplotlib : v{matplotlib.__version__}")
+    
+
