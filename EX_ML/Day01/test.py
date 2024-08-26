9d8b2f84c231606911e93282252ea09772f5d454
WonRyeong1207 <hadesdis1207@gmail.com>
2024-08-26 10:39:03 +0900

    import 확인

---
+++ EX_ML/Day01/test.py
@@ -0,0 +1,16 @@
+# 라이브러리의 여부를 알기 위한 파일
+
+import numpy
+import scipy
+import matplotlib
+import pandas
+
+
+# import scikitlearn # ???? 왜 없어? 있는데
+import sklearn
+import statsmodels
+import nltk
+# import mlib # 이거 없고
+import cv2
+# import pytorch  # 없누?
+
