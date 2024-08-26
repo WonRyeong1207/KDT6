4e4d3087ea32e4cad5f9c03e70a1b8a9b6d95f54
WonRyeong1207 <hadesdis1207@gmail.com>
2024-08-26 11:03:40 +0900

    개인공부용

--- EX_ML/Day01/test.py
+++ EX_ML/Day01/test.py
@@ -9,9 +9,11 @@ import pandas
 # import scikitlearn # ???? 왜 없어? 있는데
 import sklearn
 import statsmodels
-import nltk
+# import nltk
 # import mlib # 이거 없고
 # import cv2    # 왜 있는데 없다고 하누
 # import pytorch  # 없누?
 
 print(sklearn.__version__)
+
+# ML과 DL은 패키지 문제로 인해서 따로 가상환경을 만들거임s
