f48a45ac2d8666da6f6df4e65197a20659597520
WonRyeong1207 <hadesdis1207@gmail.com>
2024-09-29 12:11:34 +0900

    project cnn model

---
+++ mini-project/OpenCV/cnn_model_func.py
@@ -0,0 +1,9 @@
+"""
+Animal image classification
+- image data: ./data
+- animal class: AFRICAN LEOPARD, CHEETA, LION, TIGER
+---
+- learning method: supervised learning, binary classification, transfer learning(?)
+- learning algorithm: CNN
+"""
+
