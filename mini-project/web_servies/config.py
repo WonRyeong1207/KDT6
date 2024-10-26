fe56eb186309a3bb35727e7ea22edd4dc2fb2241
WonRyeong1207 <hadesdis1207@gmail.com>
2024-10-26 14:31:51 +0900

    .

--- mini-project/web_servies/config.py
+++ mini-project/web_servies/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(__file__)
 DB_NAME = 'web_sev.db'
 
 DB_MYSQL_URL = 'mysql+pymysql://root:1234@localhost:3306/web_servies'
-# DB_MYSQL_URL = 'mysql+pymysql://root:1234@172.20.97.151:3306/web_servies'
+# DB_MYSQL_URL = 'mysql+pymysql://ryeong:1207@172.20.97.151:3306/web_servies'
 # DB_MYSQL_URL = 'mysql+pymysql://root:1234@172.20.97.151:3306/web_servies?auth_plugin=caching_sha2_password'
 DB_MARIA_URL = 'mariadb+mariadbconnector://root:root!@127.0.0.1:3308/db_ai'  # 수정: :: -> :
 
